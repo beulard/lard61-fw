53a09dc383dd3e813d6bda9a93c31e96b8f0b81c
Matthias Dubouchet <beulard@gmail.com>
2024-07-14 18:21:11 +0200

    Fix no boot issue when powering up by USB

--- lard61.h
+++ lard61.h
@@ -5,25 +5,22 @@
 #define LARD61
 
 // --- LED ---
-#ifndef PICO_DEFAULT_LED_PIN
 #define PICO_DEFAULT_LED_PIN 7
-#endif
-// no PICO_DEFAULT_WS2812_PIN
 
+// --- XOSC ---
+// For some reason, oscillator is slightly slow to start on the lard61.
+// This causes the firmware not to boot when plugging in the USB power.
+// Adding this delay multiplier fixes the issue.
+// Empirically, 4 is enough to eliminate the problem in 90% of boots,
+// so 8 should be safe.
+#define PICO_XOSC_STARTUP_DELAY_MULTIPLIER 8
 
 // --- FLASH ---
 #define PICO_BOOT_STAGE2_CHOOSE_W25Q080 1
-
-#ifndef PICO_FLASH_SPI_CLKDIV
 #define PICO_FLASH_SPI_CLKDIV 2
-#endif
-
-#ifndef PICO_FLASH_SIZE_BYTES
 #define PICO_FLASH_SIZE_BYTES (4 * 1024 * 1024)
-#endif
 
-#ifndef PICO_RP2040_B0_SUPPORTED
-#define PICO_RP2040_B0_SUPPORTED 1
-#endif
+// lard61 has RP2040 chip version 2
+#define PICO_RP2040_B0_SUPPORTED 0
 
 #endif /* _BOARDS_LARD61_H */
