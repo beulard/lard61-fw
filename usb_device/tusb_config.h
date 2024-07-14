882b7958d2e0aa9229c24f5ddc7f7ff0216519d6
Matthias Dubouchet <beulard@gmail.com>
2024-07-14 12:17:23 +0200

    Max speed = full speed

--- usb_device/tusb_config.h
+++ usb_device/tusb_config.h
@@ -41,7 +41,7 @@
 
 // RHPort max operational speed can defined by board.mk
 #ifndef BOARD_TUD_MAX_SPEED
-#define BOARD_TUD_MAX_SPEED   OPT_MODE_DEFAULT_SPEED
+#define BOARD_TUD_MAX_SPEED   OPT_MODE_FULL_SPEED
 #endif
 
 //--------------------------------------------------------------------
